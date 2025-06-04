e2ec470fc3e26ca66e83e4aac5a6a53b5e6f64f5
Yaroslav Kepko <yaroslavkepko@gmail.com>
2025-06-04 16:31:22 +0200

    Final version

--- main.c
+++ main.c
@@ -3,11 +3,68 @@
 #include "ctwl.h"
 
 
+CTWL* ctwl_cyclic_cover(CTWL* list, unsigned int period)
+{
+	if(list == NULL || list->cur == NULL)
+		return NULL;
+
+	if(period == 0)
+		return NULL;
+
+	unsigned int list_length = ctwl_length(list);
+
+	if(list_length % period != 0)
+	{ 
+		printf("Period does not divide list_length without reminder\n");
+		return NULL;
+	}
+
+	else if(list_length % period == 0)
+	{
+		CTWL* cover_list = ctwl_create_empty();
+
+		if(cover_list == NULL)
+			return NULL;
+
+		TWN* init_pos = list->cur;
+		unsigned int cover_width = list_length / period;
+		
+		for(unsigned i = 0; i < period; i++)
+		{
+			float temp_data = 0;
+			
+			for(unsigned j = 0; j < cover_width; j++)
+			{
+				temp_data += list->cur->data;
+
+				for(unsigned step = 0; step < period; step++)
+					ctwl_cursor_step_right(list);
+			}
+
+			TWN* cover_node = ctwl_insert_left(cover_list, temp_data);
+			
+			if(cover_node == NULL)
+			{
+				printf("Failed to create or insert node in cover list\n");
+				ctwl_destroy(cover_list);
+				list->cur = init_pos;
+				return NULL;
+			}
+
+			list->cur = list->cur->next;
+		}
+
+		list->cur = init_pos;
+		
+		return cover_list;
+	}
+}
+
 int main(void)
 {
 	CTWL* list = ctwl_create_empty();
 	
-	for(int i = 	1; i < 7; i++)
+	for(int i = 0; i < 6; i++)
 	{
 		ctwl_insert_right(list, (float) i);
 		list->cur = list->cur->next;
@@ -21,8 +78,11 @@ int main(void)
 	printf("Cursor: %0.2f\n\n", list->cur->data);
 	
 	CTWL* cover = ctwl_cyclic_cover(list, 1);
-
+	
 	ctwl_print_list(cover);
+	
+	printf("Length: %d\n", ctwl_length(cover));
+	printf("Cursor: %0.2f\n\n", cover->cur->data);
 
 	ctwl_destroy(cover);
 	ctwl_destroy(list);
